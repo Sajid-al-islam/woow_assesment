235f9966cff66f9fe3fb6a5377b489b1f7e54c5b
Sajid <sajid@hishabee.io>
2022-10-16 11:52:25 +0600

    first commit

--- database/migrations/2018_06_20_142042_create_posts_table.php
+++ database/migrations/2018_06_20_142042_create_posts_table.php
@@ -18,6 +18,7 @@ class CreatePostsTable extends Migration
             $table->string('title');
             $table->text('body');
             $table->integer('user_id');
+            $table->integer('status')->default(0);
             $table->timestamps();
         });
     }
